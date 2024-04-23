b7d5356c88156db22980510cbf1f7dfaeee1d467
frothywater <kokoroic@outlook.com>
2024-04-23 12:57:44 +0800

    Feed RSS

--- _config.ts
+++ _config.ts
@@ -1,27 +1,47 @@
 import lume from "lume/mod.ts";
-import nunjucks from "lume/plugins/nunjucks.ts";
 import date from "lume/plugins/date.ts";
+import favicon from "lume/plugins/favicon.ts";
+import feed from "lume/plugins/feed.ts";
+import nunjucks from "lume/plugins/nunjucks.ts";
 import redirects from "lume/plugins/redirects.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import transformImages from "lume/plugins/transform_images.ts";
 
 import markdownDigest from "./_extra/digest.ts";
 
-import { imageDimensionsFromData } from "npm:image-dimensions";
-import { join } from "https://deno.land/std@0.223.0/path/mod.ts";
 import { existsSync } from "https://deno.land/std@0.223.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.223.0/path/mod.ts";
+import { imageDimensionsFromData } from "npm:image-dimensions";
 
 const site = lume({
   src: "./src",
 });
 
 site
+  .copy("asset", "/")
   .copyRemainingFiles()
   .use(nunjucks())
   .use(date())
   .use(redirects())
   .use(slugifyUrls())
-  .use(transformImages());
+  .use(transformImages())
+  .use(favicon({ input: "/favicon.png" }))
+  .use(feed({
+    query: "type=note|log|music-log",
+    sort: "date=desc",
+    limit: 1000,
+    info: {
+      title: "=site.title",
+      description: "=site.description",
+      lang: "zh",
+    },
+    items: {
+      title: "=title",
+      description: "=digest",
+      published: "=date",
+      lang: "zh",
+    },
+  }));
 
 // Generate digest for markdown files with type of post
 site.hooks.addMarkdownItPlugin(markdownDigest);
