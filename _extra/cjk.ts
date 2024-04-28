23320bb7acece49c6da82bd49c2f42a72b938a55
frothywater <kokoroic@outlook.com>
2024-04-28 13:22:05 +0800

    Fix em dash

--- _extra/cjk.ts
+++ _extra/cjk.ts
@@ -39,13 +39,13 @@ type Segment = {
 
 const STOP_TAGS = ["BR", "RUBY", "PRE", "CODE", "IMG", "STYLE"];
 
-// ambiguous: ([/–·]+)
+// ambiguous: ([/–—·]+)
 // cjk-punc:  ([‼⁇-⁉、。〝〟！，：；？〈-】〔-〛（）]+|——|⋯⋯)
 // fullwidth: ([Ａ-Ｚａ-ｚ]+)
-// latin:     ([p{Script=Latin}0-9 -#%-+,-.:;?@[-]_{} ¡§«²³¶¹»-¿‐-‒‘’“”†‡…‧‰′-‷‹›‽-‾⁎⁗⁰⁴-⁹₀-₉⅐⅓⅔⅛-⅞Ⅰ-ⅿ⑴-⒛⸘⸮°]+)
+// latin:     ([p{Script=Latin}0-9 -#%-+,-.:;?@[-]_{} ¡§«²³¶¹»-¿‐-‒‘’“”†‡…‧‰′-‷‹›‽-‾⁎⁗⁰⁴-⁹₀-₉⅐⅓⅔⅛-⅞Ⅰ-ⅿ⑴-⒛⸘⸮°\|]+)
 // kana:      ([ぁ-ゟ゠-ヺーヿ]+)
 const SEGMENT_REGEX =
-  /([\u002f\u2013\u00b7]+)|([\u203c\u2047-\u2049\u3001\u3002\u301d\u301f\uff01\uff0c\uff1a\uff1b\uff1f\u3008-\u3011\u3014-\u301B\uff08\uff09]+|\u2014\u2014|\u22ef\u22ef)|([\uff21-\uff3a\uff41-\uff5a]+)|([\p{Script=Latin}0-9\u0020-\u0023\u0025-\u002b\u002c-\u002e\u003a\u003b\u003f\u0040\u005b-\u005d\u005f\u007b\u007d\u00a0\u00a1\u00a7\u00ab\u00b2\u00b3\u00b6\u00b9\u00bb-\u00bf\u2010-\u2012\u2018\u2019\u201c\u201d\u2020\u2021\u2026\u2027\u2030\u2032-\u2037\u2039\u203a\u203d-\u203e\u204e\u2057\u2070\u2074-\u2079\u2080-\u2089\u2150\u2153\u2154\u215b-\u215e\u2160-\u217f\u2474-\u249b\u2e18\u2e2e\u00b0]+)|([\u3041-\u309f\u30a0-\u30fa\u30fc\u30ff]+)/gu;
+  /([\u002f\u2013\u2014\u00b7]+)|([\u203c\u2047-\u2049\u3001\u3002\u301d\u301f\uff01\uff0c\uff1a\uff1b\uff1f\u3008-\u3011\u3014-\u301B\uff08\uff09]+|\u2014\u2014|\u22ef\u22ef)|([\uff21-\uff3a\uff41-\uff5a]+)|([\p{Script=Latin}0-9\u0020-\u0023\u0025-\u002b\u002c-\u002e\u003a\u003b\u003f\u0040\u005b-\u005d\u005f\u007b\u007d\u00a0\u00a1\u00a7\u00ab\u00b2\u00b3\u00b6\u00b9\u00bb-\u00bf\u2010-\u2012\u2018\u2019\u201c\u201d\u2020\u2021\u2026\u2027\u2030\u2032-\u2037\u2039\u203a\u203d-\u203e\u204e\u2057\u2070\u2074-\u2079\u2080-\u2089\u2150\u2153\u2154\u215b-\u215e\u2160-\u217f\u2474-\u249b\u2e18\u2e2e\u00b0\u007c]+)|([\u3041-\u309f\u30a0-\u30fa\u30fc\u30ff]+)/gu;
 
 // dot: ([。、！，：；？])
 const DOT_PUNC_REGEX = /[\u3001\u3002\uff01\uff0c\uff1a\uff1b\uff1f]/;
@@ -134,8 +134,8 @@ function correctAmbiguousPunc(body: Element) {
         // Assume latin for single segment
         el.classList.add(SegmentClass.latin);
       } else if (
-        el.previousElementSibling.classList.contains(SegmentClass.latin) &&
-        el.nextElementSibling.classList.contains(SegmentClass.latin)
+        el.previousElementSibling?.classList.contains(SegmentClass.latin) &&
+        el.nextElementSibling?.classList.contains(SegmentClass.latin)
       ) {
         // Assume latin between latin segments
         el.classList.add(SegmentClass.latin);
@@ -144,7 +144,7 @@ function correctAmbiguousPunc(body: Element) {
         // Force full width slash
         el.innerHTML = "／";
         el.classList.add(SegmentClass.cjkPunc, PuncClass.connector);
-      } else if (text === "–") {
+      } else if (text === "–" || text === "—") {
         // CJK dash U+2013 -> U+2014
         // Force full width dash
         el.innerHTML = "—";
